bb2fe0cdf5137b309bd7d86f78dc4c095a7013c0
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-30 16:53:49 -0400

    Add a body-parser as a dependency and setup it so as to convert the body into string

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
