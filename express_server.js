141f287585de1192ed047bdbc16697eb887408fe
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-31 13:36:56 -0400

    Add delete buttons for each url, add a route to delete the resource and redirects back to My URL's page (/urls)

--- express_server.js
+++ express_server.js
@@ -40,9 +40,15 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 
 function generateRandomString() {
