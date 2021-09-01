ac59a7cb643a30b7c45e3d047746959cbed6992d
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-09-01 13:33:58 -0400

    Create registration template and /register endpoint to display the registration page

--- express_server.js
+++ express_server.js
@@ -71,6 +71,11 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
+app.get('/register', (req, res) => {
+  const templateVars = { username: req.cookies['username'] };
+  res.render('register', templateVars);
+});
+
 function generateRandomString() {
   return Math.random().toString(36).substring(2,7);
 }
